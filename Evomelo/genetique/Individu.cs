a62a62a769200d75731fc18145d79fcc1a5062e3
jeremy <a>
2016-01-15 14:25:03 +0100

    algo genetique

--- Evomelo/genetique/Individu.cs
+++ Evomelo/genetique/Individu.cs
@@ -4,10 +4,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Evomelo.genetique
+namespace Evomelo.Genetique
 {
     class Individu
     {
-        private int[] piste = new int[16];
+        private int[] _notes;
+        private int _fitness;
+        private int _instrument;
+
+        //initialisation
+        public Individu(int nbNotes)
+        {
+            _notes = new int[nbNotes];
+            _instrument = new Random().Next(1, 128);
+            _fitness = 0;
+        }
+
+        public int[] notes
+        {
+            get
+            {
+                return this._notes;
+            }
+            set
+            {
+                this._notes = value;
+            }
+        }
+
+        //la note utilisateurs
+        public int fitness
+        {
+            get
+            {
+                return this._fitness;
+            }
+            set
+            {
+                this._fitness = value;
+            }
+        }
+
+        //génère une piste de façon aléatoire
+        public void generateRandomNotes()
+        {
+            for(int i = 0; i < _notes.Length; i++)
+            {
+                _notes[i] = new Random().Next(0, 127);
+            }
+        }
     }
 }
--- Evomelo/genetique/Population.cs
+++ Evomelo/genetique/Population.cs
@@ -4,10 +4,150 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Evomelo.genetique
+namespace Evomelo.Genetique
 {
     class Population
     {
-        private Individu[] individus = new Individu[15];
+        private double _tauxCross = 0.6;
+        private int _nbIndividu = 10;
+        private int _nbNotes = 16;
+        private double _tauxMut;
+        private double _tauxSurvie = 2.5;
+
+        private Individu[] _individus;
+
+        public Population()
+        {
+            _individus = new Individu[_nbIndividu];
+            _tauxCross = 1 / _nbNotes;
+        }
+
+        //pour accéder à la liste depuis l'interface
+        public Individu[] individus
+        {
+            get
+            {
+                return this._individus;
+            }
+            set
+            {
+                this._individus = value;
+            }
+        }
+
+        //fonction de cross-over
+        private Individu cross(Individu i1, Individu i2)
+        {
+            Individu newI = new Individu(_nbNotes);
+            int[] tabNote = new int[_nbNotes];
+            int nbSeq = (int)(_nbNotes * new Random().NextDouble());
+
+            for (int i = 0; i < _nbNotes; i++)
+            {
+                if (i < nbSeq)
+                {
+                    tabNote[i] = i1.notes[i];
+                }
+                else
+                {
+                    tabNote[i] = i2.notes[i];
+                }
+            }
+
+            newI.notes = tabNote;
+
+            return newI;
+        }
+
+        //fonction de mutation
+        private Individu mutation(Individu i1)
+        {
+            for (int i = 0; i < _nbNotes; i++)
+            {
+                double random = new Random().NextDouble();
+                if (random <= _tauxMut)
+                {
+                    i1.notes[i] = new Random().Next(0,127);
+                }
+            }
+
+            return i1;
+        }
+
+        //sélectionne un individu
+        private Individu selection()
+        {
+            int p1 = new Random().Next(0, 10);
+            int p2 = new Random().Next(0, 10);
+            Individu i;
+
+            if(_individus[p1].fitness <= _individus[p2].fitness)
+            {
+                i = _individus[p2];
+            }
+            else
+            {
+                i = _individus[p1];
+            }
+
+            return i;
+        }
+
+        //génère une nouvelle génération
+        public void newGeneration()
+        {
+            Individu[] newPop = new Individu[_nbIndividu];
+            Individu individu;
+            Individu individu2;
+            int i = 0;
+
+            individu = selectElite();
+            if(individu != null)
+            {
+                newPop[i] = individu;
+                i++;
+            }
+
+            for(; i < _nbIndividu; i++)
+            {
+                individu = selection();
+                
+                //si cross
+                if (_tauxCross > new Random().NextDouble())
+                {
+                    individu2 = selection();
+                    individu = cross(individu, individu2);
+                }
+
+                individu = mutation(individu);
+
+                newPop[i] = individu;
+            }
+
+            _individus = newPop;
+        }
+
+        //principe de survie si la moyenne des note est >= _tauxSurvie
+        private Individu selectElite()
+        {
+            Individu elite = new Individu(_nbNotes);
+            int sFitness = 0;
+
+            for (int i = 0; i < _nbIndividu; i++)
+            {
+                sFitness = sFitness + _individus[i].fitness;
+                if(_individus[i].fitness > elite.fitness)
+                {
+                    elite = _individus[i];
+                }
+            }
+
+            if(sFitness/_nbIndividu < _tauxSurvie)
+            {
+                elite = null;
+            }
+
+            return elite;
+        }
     }
 }
